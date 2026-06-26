3c88e3d77836d7ff76ee7d046637f77abcacb56b
Martin I GL <tugaskuliahmartin@gmail.com>
2026-06-27 02:32:10 +0700

    Menambahkan Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,42 @@
+import heroImage from "../assets/images/images.jpg";
+
 function Hero() {
   return (
-    <section id="home">
-      <h1>Halo, Saya Martin I GL</h1>
+    <section className="hero" id="home">
 
-      <p>
-        Mahasiswa Informatika yang fokus
-        pada Frontend Development dan UI/UX Design.
-      </p>
+      <div className="hero-text">
 
-      <button>Tentang Saya</button>
+        <p className="greeting">Halo, Saya</p>
+
+        <h1>Martin I GL</h1>
+
+        <h3>
+          Mahasiswa Informatika <br />
+          Frontend Developer <br />
+          UI/UX Designer
+        </h3>
+
+        <p className="description">
+          Saya memiliki minat dalam pengembangan website,
+          desain UI/UX, serta pengembangan aplikasi menggunakan Java.
+        </p>
+
+        <div className="hero-button">
+          <a href="#contact" className="btn-primary">
+            Hubungi Saya
+          </a>
+
+          <a href="#projects" className="btn-secondary">
+            Lihat Project
+          </a>
+        </div>
+
+      </div>
+
+      <div className="hero-image">
+        <img src={heroImage} alt="Hero" />
+      </div>
+
     </section>
   );
 }
